325c0d903674c3ce0011c5377415f2dc897bd4b0
Chiedozie Lawrence <chiedozieonyekwelu989@gmail.com>
2025-10-10 20:17:33 +0100

    Split header in ro a components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackPage } from './pages/TrackPage';
+
 
 function App() {
   return (
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="checkout" element={<CheckoutPage />} />
-      </Routes>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path='orders' element={<OrdersPage />} />
+      <Route path='track' element={<TrackPage />} />
+    </Routes>
   );
 }
 
